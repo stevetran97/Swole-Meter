4235ffc71aba121d1c6dc284429b34428da3684b
samly97 <97.sam.ly@gmail.com>
2020-09-10 16:26:23 -0400

    Successfully tested fetching data from Golang API server.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    loading: true,
+    workouts: [],
+  };
+
+  async componentDidMount() {
+    const url = "http://localhost:8080/api/workouts";
+    const response = await fetch(url, {
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        Accept: "json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await response.json();
+
+    this.setState({
+      loading: false,
+      workouts: data,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <table>
+              <tr>
+                <th>Workout ID</th>
+                <th>Date</th>
+              </tr>
+              {this.state.workouts.map((workout) => (
+                <tr key={workout.ID}>
+                  <td>{workout.ID}</td>
+                  <td>{workout.date}</td>
+                </tr>
+              ))}
+            </table>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
-
-export default App;
