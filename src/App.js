b81a86a702acf4c2514682be9e2a6a805e3077d9
samly97 <97.sam.ly@gmail.com>
2020-09-11 10:06:21 -0400

    test multiple fetch... added fetch from GET prs api

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ export default class App extends React.Component {
   state = {
     loading: true,
     workouts: [],
+    prs: [],
   };
 
   async componentDidMount() {
-    const url = "http://localhost:8080/api/workouts";
-    const response = await fetch(url, {
+    const urlWorkouts = "http://localhost:8080/api/workouts";
+    const workoutResponse = await fetch(urlWorkouts, {
       mode: "cors",
       credentials: "include",
       headers: {
@@ -17,11 +18,24 @@ export default class App extends React.Component {
       },
     });
 
-    const data = await response.json();
+    const workoutData = await workoutResponse.json();
+
+    const urlPrs = "http://localhost:8080/api/prs";
+    const prResponse = await fetch(urlPrs, {
+      mode: "cors",
+      credentials: "include",
+      headers: {
+        Accept: "json",
+        "Content-Type": "application/json",
+      },
+    });
+
+    const prData = await prResponse.json();
 
     this.setState({
       loading: false,
-      workouts: data,
+      workouts: workoutData,
+      prs: prData,
     });
   }
 
@@ -32,17 +46,47 @@ export default class App extends React.Component {
           <div>Loading...</div>
         ) : (
           <div>
+            <h1>Your workout history</h1>
             <table>
-              <tr>
-                <th>Workout ID</th>
-                <th>Date</th>
-              </tr>
-              {this.state.workouts.map((workout) => (
-                <tr key={workout.ID}>
-                  <td>{workout.ID}</td>
-                  <td>{workout.date}</td>
+              <thead>
+                <tr>
+                  <th>Workout ID</th>
+                  <th>Date</th>
                 </tr>
-              ))}
+              </thead>
+              <tbody>
+                {this.state.workouts.map((workout) => (
+                  <tr key={workout.ID}>
+                    <td>{workout.ID}</td>
+                    <td>{workout.date}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        {this.state.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <h1>Your Records</h1>
+            <table>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Exercise</th>
+                  <th>Weight</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.prs.map((pr) => (
+                  <tr key={pr.pr_id}>
+                    <td>{pr.date}</td>
+                    <td>{pr.exercise}</td>
+                    <td>{pr.weight}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
           </div>
         )}
