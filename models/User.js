cef5790f4530bf869e0413cac378a946cf2ef321
stevetran97 <65732396+stevetran97@users.noreply.github.com>
2021-08-20 13:42:27 -0400

    Exercise model added. Route entry point into app.js is now facilitated by routes/index.js

--- models/User.js
+++ models/User.js
@@ -3,34 +3,39 @@ const mongoose = require("mongoose");
 // Passport for including User entry in the request via authentification
 const passportLocalMongoose = require("passport-local-mongoose");
 
-let UserSchema = new mongoose.Schema({
-  _id: mongoose.Types.ObjectId,
-  username: String, 
-  firstName: String,
-  lastName: String,
-  password: String,
-  profile: String,
-  workouts: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Workout"
+let UserSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Types.ObjectId,
+    username: String, 
+    firstName: String,
+    lastName: String,
+    password: String,
+    profile: String,
+    workouts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Workout"
+      }
+    ],
+    personal_records: {
+      one_rep: {
+        best_squat_weight: Number,
+        best_bench_weight: Number,
+        best_deadlift_weight: Number,
+        best_shoulderp_weight: Number
+      },
+      multi_rep: {
+        best_squat_work: Number,
+        best_bench_work: Number,
+        best_deadlift_work: Number,
+        best_shoulderp_work: Number
+      }
     }
-  ],
-  personal_records: {
-    one_rep: {
-      best_squat_weight: Number,
-      best_bench_weight: Number,
-      best_deadlift_weight: Number,
-      best_shoulderp_weight: Number
-    },
-    multi_rep: {
-      best_squat_work: Number,
-      best_bench_work: Number,
-      best_deadlift_work: Number,
-      best_shoulderp_work: Number
-    }
+  }, 
+  {
+    timestamps: true
   }
-});
+);
 
 UserSchema.plugin(passportLocalMongoose);
 
--- models/Workout.js
+++ models/Workout.js
@@ -1,20 +1,22 @@
+// Each user has many workouts
 const mongoose = require("mongoose");
 
-let WorkoutSchema = new mongoose.Schema({
-  _id: mongoose.Types.ObjectId,
-  time: Date,
-  weight: Number,
-  reps: Number,
-  sets: Number,
+let WorkoutSchema = new mongoose.Schema(
+  {  
+    _id: mongoose.Types.ObjectId,
+    time: Date,
 
-  type: String, //ONE_REP or MULTI_REP
-  creator: {
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }
-  }
-});
+    type: String, //ONE_REP or MULTI_REP
+    creator: {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    },
+  },
+  {
+    timestamps: true
+  });
 
 let Workout = mongoose.model("Workout", WorkoutSchema);
 module.exports = Workout;
